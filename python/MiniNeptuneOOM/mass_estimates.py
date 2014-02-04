4abc35a50e49daadc0bad7f4283dd2037bcda2b1
apiso <anamaria.piso@gmail.com>
2014-02-04 17:33:16 -0500

    python files

---
+++ python/MiniNeptuneOOM/mass_estimates.py
@@ -0,0 +1,448 @@
+from utils.constants import G, AU, Me, Re, Msun, kb, Rfn
+import numpy as np
+from scipy.optimize import brentq
+
+def Tdisk(a, b, FT):
+    
+    """
+    Disk temperature
+
+    Input
+    ----
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis dependence
+    FT:
+        normalization factor relative to the MMSN
+
+    Output:
+    -------
+    Tdisk:
+        disk temperature in K
+    
+    """
+
+    return 300 * FT * a**(-b)
+
+
+def RBondi(mc, a, b, FT, Y = 0.3):
+
+    """
+    Bondi radius
+
+    Input:
+    -----
+    mc:
+        core mass in Earth masses
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis dependence
+    FT:
+        normalization factor relative to the MMSN
+    Y:
+        Helium mass fraction (default 0.3)
+
+    Output
+    ------
+    Bondi radius in cm
+        
+    """
+
+    return G * mc * Me / (Rfn(Y) * Tdisk(a, b, FT))
+
+
+def RHill(mc, a, mstar):
+
+    """
+    Hill radius
+
+    Input
+    -----
+    mc:
+        core / planet mass in Earth masses
+    a:
+        semi-major axis in AU
+    mstar:
+        central star mass in Solar masses
+
+    Output
+    ------
+    Hill radius in cm
+    
+    """
+
+    return a * AU * (mc * Me / (3 * mstar * Msun))**(1./3)
+
+
+def sigmadisk(FSigma, a, d):
+
+    """
+
+    gas surface density in cm^-2
+
+    Input
+    -----
+    FSigma:
+        normalization relative to the MMSN
+    a:
+        semi-major axis in AU
+    d:
+        power-law index in semi-major axis dependence
+
+    Output
+    ------
+    Simgad
+
+    """
+
+    return 2 * 10**3 * FSigma * a**(-d)
+
+
+
+
+
+
+def cdisk(a, b, FT, Y = 0.3):
+
+    """
+
+    isothermal sound speed
+
+    Input
+    -----
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis dependence
+    FT:
+        normalization factor relative to the MMSN
+    Y:
+        Helium mass fraction (default 0.3)
+
+    Output
+    ------
+    cdisk in cm s^-1
+
+    """
+
+    return (Rfn(0.3) * Tdisk(a, b, FT))**(1./2)
+
+
+def Omega(a, mstar):
+
+    """
+
+    Keplerian angular velocity
+
+    Input
+    -----
+    a:
+        semi-major axis in AU
+    mstar:
+        central star mass in solar masses
+
+    Output
+    ------
+    Omega in s^-1
+    
+    """
+
+    return (G * mstar * Msun / (a * AU)**3)**(1./2)
+
+
+def sigmamax(a, b, FT, mstar, Y = 0.3):
+
+    """
+
+    maximal surface density in cm^-2 for which the disk is gravitationally stable (Q = 1)
+
+    Input
+    -----
+
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis dependence
+    FT:
+        normalization factor relative to the MMSN
+    mstar:
+        central star mass in Solar masses
+    Y:
+        Helium mass fraction (default 0.3)
+
+
+    Output
+    ------
+    Simgamax in cm^-2
+
+    """
+
+    return cdisk(a, b, FT, Y) * Omega(a, mstar) / (np.pi * G)
+
+
+def Hdisk(a, b, FT, mstar, Y = 0.3):
+
+    """
+
+    disk scale height
+
+    Input
+    -----
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis dependence
+    FT:
+        normalization factor relative to the MMSN
+    mstar:
+        central star mass in solar masses
+    Y:
+        Helium mass fraction (default 0.3)
+
+    Output
+    ------
+    disk scale height in cm
+
+    """
+    
+    return cdisk(a, b, FT, Y) / Omega(a, mstar)
+
+
+def rhodisk(a, b, d, FT, FSigma, mstar, Y = 0.3, maxQ = 0):
+
+    """
+
+    disk mid-plane density
+
+    Input
+    -----
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis temperature dependence
+    d:
+        power-law index in semi-major axis surface density dependence
+    FT:
+        temperature normalization factor relative to the MMSN
+    FSigma:
+        surface density normalization factor relative to the MMSN
+    mstar:
+        central star mass in solar masses
+    Y:
+        Helium mass fraction (default 0.3)
+
+    Output
+    ------
+    rhodisk in g cm^-3
+
+
+    """
+
+    if maxQ == 0:
+        return sigmadisk(FSigma, a, d) / (2 * Hdisk(a, b, FT, mstar, Y))
+    else:
+        return sigmamax(a, b, FT, mstar, Y) / (np.sqrt(2 * np.pi) * Hdisk(a, b, FT, mstar, Y))
+
+
+def mtrans(a, b, FT, mstar, Y = 0.3):
+
+    """
+
+    mass for which RBondi = RHill
+
+    Input
+    -----
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis temperature dependence
+    FT:
+        temperature normalization factor relative to the MMSN
+
+    mstar:
+        central star mass in Solar masses
+    Y:
+        Helium mass fraction (default 0.3)
+
+    Output
+    ------
+    mtrans in g
+    
+    """
+
+    def f(mc):
+        return RBondi(mc, a, b, FT, Y) - RHill(mc, a, mstar)
+    
+    return brentq(f, 1e-10, 1000) * Me
+
+
+def mthermal(a, b, FT, mstar, Y = 0.3):
+
+    """
+
+    mass for which RHill = Hdisk
+
+    Input
+    -----
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis temperature dependence
+    FT:
+        temperature normalization factor relative to the MMSN
+    mstar:
+        central star mass in solar masses
+    Y:
+        Helium mass fraction (default 0.3)
+
+    Output
+    ------
+    mthermal in g
+    
+    """
+
+    def f(mc):
+        return RHill(mc, a, mstar) - Hdisk(a, b, FT, mstar, Y)
+    
+    return brentq(f, 1e-10, 1000) * Me
+    
+    
+
+def MBondi(mc, a, b, d, FT, FSigma, mstar, Y = 0.3, maxQ = 0):
+
+    """
+
+    atmosphere mass inside Bondi radius as a fraction of the core mass
+
+    Input
+    -----
+    mc:
+        core mass in Earth masses
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis temperature dependence
+    d:
+        power-law index in semi-major axis surface density dependence
+    FT:
+        temperature normalization factor relative to the MMSN
+    FSigma:
+        surface density normalization factor relative to the MMSN
+    mstar:
+        central star mass in solar masses
+    Y:
+        Helium mass fraction (default 0.3)
+
+    Output
+    ------
+    MBondi in units of core masses
+
+    """
+
+    return 4 * np.pi * \
+           rhodisk(a, b, d, FT, FSigma, mstar, Y, maxQ) * RBondi(mc, a, b, FT, Y)**3 / (mc * Me)
+
+
+def MHill(a, b, d, FT, FSigma, mstar, Y = 0.3, maxQ = 0):
+
+    """
+
+    atmosphere mass inside Hill radius as a fraction of core mass
+
+    Input
+    -----
+    mc:
+        core mass in Earth masses
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis temperature dependence
+    d:
+        power-law index in semi-major axis surface density dependence
+    FT:
+        temperature normalization factor relative to the MMSN
+    FSigma:
+        surface density normalization factor relative to the MMSN
+    mstar:
+        central star mass in solar masses
+    Y:
+        Helium mass fraction (default 0.3)
+
+    Output
+    ------
+    MHill in units of core mass
+
+    """
+
+    return 4 * np.pi * \
+           rhodisk(a, b, d, FT, FSigma, mstar, Y, maxQ) * (a * AU)**3 / (3 * mstar * Msun)
+
+
+
+#---------------------------------------------------------------------------------------
+
+def Matm(mc, a, b, d, FT, FSigma, mstar, Y = 0.3, maxQ = 0):
+
+    """
+
+    atmosphere mass 
+
+    Input
+    -----
+    mc:
+        core mass in Earth masses
+    a:
+        semi-major axis in AU
+    b:
+        power-law index in semi-major axis temperature dependence
+    d:
+        power-law index in semi-major axis surface density dependence
+    FT:
+        temperature normalization factor relative to the MMSN
+    FSigma:
+        surface density normalization factor relative to the MMSN
+    mstar:
+        central star mass in solar masses
+    Y:
+        Helium mass fraction (default 0.3)
+
+    Output
+    ------
+    Matm in units of core masses
+
+    """
+
+    if RBondi(mc, a, b, FT, Y) <= RHill(mc, a, mstar):
+        return MBondi(mc, a, b, d, FT, FSigma, mstar, Y, maxQ)
+    else:
+        return MHill(a, b, d, FT, FSigma, mstar, Y, maxQ)
+
+#----------------------------------------------------------------------------------------------
+
+## attempt to find mass-radius relationship for atmosphere from basic principles
+
+def Ratm(Mc, rhoc, f, T, kappa):
+
+    Rc = (3 * Mc / (4 * np.pi * rhoc))**(1./3)
+
+    def f(Ratm):
+        return 4 * np.pi * G * (f + 1) * Ratm**3 / (Rfn(0.3) * T * kappa * (Rc + Ratm)**2) - f
+
+    return brentq(f, 1e-10, 1e35) * Re
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ python/utils/constants.py
@@ -0,0 +1,141 @@
+"""
+
+Constants for atmosphere modelling ; cgs unless otherwise noted
+
+"""
+import numpy as np
+from collections import namedtuple
+
+#physical
+G = 6.673e-08
+c = 2.99792458e10
+h = 6.626068e-27
+kb = 1.3807e-16
+mp = 1.67262171e-24
+mH = mp
+NA = 6.02214129e23
+Rb = 83144720.0 #NA*kb #ideal gas constant (per mol not per H!)
+RH = Rb/2
+RHe = Rb/4
+sigma =  5.670373e-5
+
+#unit conversion
+cmperau = 14959788700000.0
+AU = 1.49597887e13
+year = 3.16e7
+Myr = 3.16e13
+barP = 1e6 #dyne/cm*2 to bar (not that `bar` in matplotlib is problem in pylab)
+
+#astronomical
+Me = 5.9742e27
+Re = 6.371e8
+Msun = 1.9891e33
+
+def RHill(mp, a, Ms = Msun):
+    """
+    Hill radius in cgs with SMA (`a`) in AU and planet mass (`mp`) also
+    in cgs
+    """
+    return (mp / 3 / Ms)**(1./3) * a * AU
+
+#MMSN disk model
+#(could/ should make the deviations from standard MMSN optional or keyword parameters)
+def Tdisk(a, FT):  
+    """Disk temperature as function of semi-major axis (a in AU) and Ft"""
+    return 120 * FT * (a)**(-3./7)
+
+def Pdisk(a, mstar, FSigma, FT): 
+    """
+    Disk pressure (in dyn cm^-2) as function of semi-major axis (a in AU),
+    stellar mass (mstar in solar masses), and factors Fsigma and Ft
+    """
+    return 1.1 * 10**(-4) * FSigma * np.sqrt(FT * mstar) * a**(-45./14) * 10**6
+
+def Hdisk(a, FT):  #a in AU
+    """Disk scaleheight (in cm) as function of semi-major axis and Ft"""
+    return 0.021784 * AU * np.sqrt(FT) * (a)**(9./7) 
+
+#opacity laws
+
+def kdust(T, rho = None, b = 2, fsolar = 1.):
+    """Opacity (generally applicable for beta = 2)"""
+    return 2 * fsolar * (T/100.)**b
+
+def kdustall(T, rho):
+    T = float(T)
+    rho = float(rho)
+    """Full Bell & Lin opacity"""
+    if T <= 166.81:
+        return 2 * 10**(-4) * T**2
+    elif T >= 166.81 and T <= 201.677:
+        return 2 * 10**16 * T**(-7)
+    elif T >= 201.677 and 0.1 * T**(1./2) - 2 * 10**81 * rho * T**(-24) <=0:
+        return 0.1 * T**(1./2)
+    elif 0.1 * T**(1./2) - 2 * 10**81 * rho * T**(-24) >=0 \
+         and 2 * 10**81 * rho * T**(-24) - 10**(-8) * rho**(2./3) * T**3 >= 0:
+        return 2 * 10**81 * rho * T**(-24)
+    elif 2 * 10**81 * rho * T**(-24) - 10**(-8) * rho**(2./3) * T**3 <=0 \
+         and 10**(-8) * rho**(2./3) * T**3 - 10**(-36) * rho**(1./3) * T**10 >= 0:
+        return 10**(-8) * rho**(2./3) * T**3
+    elif 10**(-8) * rho**(2./3) * T**3 - 10**(-36) * rho**(1./3) * T**10 <= 0 \
+         and 10**(-36) * rho**(1./3) * T**10 - 1.5 * 10**20 * rho * T**(-5./2) <= 0:
+        return 10**(-36) * rho**(1./3) * T**10
+    elif 10**(-36) * rho**(1./3) * T**10 - 1.5 * 10**20 * rho * T**(-5./2) >= 0 \
+         and 1.5 * 10**20 * rho * T**(-5./2) - 0.348 >= 0:
+        return 1.5 * 10**20 * rho * T**(-5./2) 
+    else:
+         return 0.348
+
+def kdustallfn(T, rho):
+    kappa = 0 * np.ndarray(shape = len(rho), dtype = float)
+    for i in range(len(kappa)):
+        kappa[i] = kdustall(T, rho[i])
+    return kappa
+
+def kdustbeta1(T, a = None, FT = 1):
+    return 0.1 * (T/100.0)
+
+def kdust10(T, P = None, b = 2, fsolar = 1.):
+    """Opacity reduced by a factor of 10"""
+    return 2 * fsolar * (T/100.)**b / 10
+
+def kdust100(T, P = None, b = 2, fsolar = 1.):
+    """Opacity reduced by a factor of 100"""
+    return 2 * fsolar * (T/100.)**b / 100
+
+def kfixed(T = None, P = None, const = 0.24):
+    """a silly function but useful if kappa functions expects a temperature."""
+    return const
+
+def gammafn(delad):
+    """Polytropic index gamma as function of delad."""
+    return 1. /(1 - delad) 
+
+#R, Cv, molecular weight for a given H/He mixture fraction Y
+
+def mufn(Y):
+    return 4./(2-Y)
+
+def Rfn(Y):
+    return Rb / mufn(Y)
+
+def Cvfn(Y, delad):
+    """
+    Assumes that the He fraction only affects mu, but not delad.
+    """
+    return Rfn(Y) / (gammafn(delad) - 1)
+
+#tuple of gas, disk and core parameters for polytrope
+params = namedtuple( \
+    'params', 'Mco, rco, a, delad, Y, gamma, R, Cv, Pd, Td, kappa')
+
+#tuple of gas, disk and core parameters for real EOS
+paramsEOS = namedtuple( \
+    'paramsEOS', 'Mco, rco, Y, a, Pd, Td, kappa')
+
+#tuple of gas, disk and core parameters for polytrope with two delad
+paramsvar = namedtuple( \
+    'params', 'Mco, rco, a, delad1, delad2, Y, gamma1, gamma2, R, Cv1, Cv2, Pd, Td, kappa')
+
+
+
