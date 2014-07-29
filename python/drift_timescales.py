a021df37fa1aafc7f2067b082e1caceed3a3c589
apiso <anamaria.piso@gmail.com>
2014-07-29 18:42:23 -0400

    update

--- python/drift_timescales.py
+++ python/drift_timescales.py
@@ -4,6 +4,7 @@ from T_freeze import T_freeze, Rdes, tevap, vib_freq
 from C_to_O import T_freeze_H20, T_freeze_CO2, T_freeze_CO
 from scipy.integrate import odeint
 from scipy.optimize import brentq
+from scipy.interpolate import interp1d
 
 def Tdisk(r, T0 = 120, betaT = 3./7):
      """Disk temperature in K with r in AU"""
@@ -37,21 +38,29 @@ def lambdamfp(r, Sigma0 = 2200, betaS = 3./2, T0 = 120, betaT = 3./7, mu = 2.35,
         (rhodisk(r, Sigma0, betaS, T0, betaT, mu, Mstar) / (mu * mp)))
     
 def ts(r, s, rhos = 3.0, T0 = 120, betaT = 3./7, mu = 2.35, Sigma0 = 2200, betaS = 3./2, \
-    Mstar = Msun, sigma = 2 * 10**(-15)):
+    Mstar = Msun, sigma = 2 * 10**(-15), eps = 0):
     """Stopping time in seconds with r in AU and s in cm"""
-    if s <= 9 * lambdamfp(r, Sigma0, betaS, T0, betaT, mu) / 4:
-       
-        return rhos * s / (rhodisk(r, Sigma0, betaS, T0, betaT, mu, Mstar) * \
-            cdisk(r, T0, betaT, mu))
-            
-    else:
-        return 4 * rhos * s**2 / (9 * rhodisk(r, Sigma0, betaS, T0, betaT, mu, Mstar) * \
-            cdisk(r, T0, betaT, mu) * lambdamfp(r, Sigma0, betaS, T0, betaT, mu, Mstar, sigma))
+
+    if eps == 0:
+         if s <= 9 * lambdamfp(r, Sigma0, betaS, T0, betaT, mu) / 4:
+
+            return rhos * s / (rhodisk(r, Sigma0, betaS, T0, betaT, mu, Mstar) * \
+                 cdisk(r, T0, betaT, mu))
+                 
+         else:
+              return 4 * rhos * s**2 / (9 * rhodisk(r, Sigma0, betaS, T0, betaT, mu, Mstar) * \
+                 cdisk(r, T0, betaT, mu) * lambdamfp(r, Sigma0, betaS, T0, betaT, mu, Mstar, sigma))
+    elif eps == 1:
+         return rhos * s / (rhodisk(r, Sigma0, betaS, T0, betaT, mu, Mstar) * \
+                 cdisk(r, T0, betaT, mu))
+    elif eps == -1:
+         return 4 * rhos * s**2 / (9 * rhodisk(r, Sigma0, betaS, T0, betaT, mu, Mstar) * \
+                 cdisk(r, T0, betaT, mu) * lambdamfp(r, Sigma0, betaS, T0, betaT, mu, Mstar, sigma))
         
 def taus(r, s, rhos = 3.0, T0 = 120, betaT = 3./7, mu = 2.35, Sigma0 = 2200, betaS = 3./2, \
-    Mstar = Msun, sigma = 2 * 10**(-15)):
+    Mstar = Msun, sigma = 2 * 10**(-15), eps = 0):
     """Dimensionless stopping time with r in AU and s in cm"""
-    return ts(r, s, rhos, T0, betaT, mu, Sigma0, betaS, Mstar, sigma) * Omegak(r, Mstar) 
+    return ts(r, s, rhos, T0, betaT, mu, Sigma0, betaS, Mstar, sigma, eps) * Omegak(r, Mstar) 
 
 def n(betaS = 3./2, betaT = 3./7):
     """Power law coefficient in P \propto r^(-n)"""
@@ -63,13 +72,12 @@ def eta(r, T0 = 120., betaT = 3./7, mu = 2.35, Mstar = Msun, betaS = 3./2):
         (2 * Omegak(r, Mstar) * r * cmperau)**2  
         
 def tr(r, s, rhos = 3.0, T0 = 120, betaT = 3./7, mu = 2.35, Sigma0 = 2200, betaS = 3./2, \
-    Mstar = Msun, sigma = 2 * 10**(-15)):
+    Mstar = Msun, sigma = 2 * 10**(-15), eps = 0):
     """Radial drift time in seconds with r in AU and s in cm"""
     return (1 + taus(r, s, rhos, T0, betaT, mu, Sigma0, betaS, \
-        Mstar, sigma)**2)/ \
+        Mstar, sigma, eps)**2)/ \
             taus(r, s, rhos, T0, betaT, mu, Sigma0, betaS, \
-                Mstar, sigma) * np.sqrt(G * Mstar * r * cmperau) / \
-                    cdisk(r, T0, betaT, mu)**2
+                Mstar, sigma, eps) / (2 * eta(r, T0, betaT, mu, Mstar, betaS) * Omegak(r, Mstar))
 
 def rdot(r, s, rhos = 3.0, T0 = 120, betaT = 3./7, mu = 2.35, Sigma0 = 2200, betaS = 3./2, \
     Mstar = Msun, sigma = 2 * 10**(-15)):
@@ -100,21 +108,26 @@ def rf(rin, sin, mx, Ex, Nx = 1e15, rhos = 3.0, T0 = 120, betaT = 3./7, mu = 2.3
                rdot(x[0] / cmperau, x[1], rhos, T0, betaT, mu, Sigma0, betaS, Mstar, sigma), \
                - 3 * mx * mp  / rhos * \
                     Nx * Rdes(mx, Ex, Tdisk(x[0] / cmperau, T0, betaT))])
-     sf = sin
-     
-     #while(sf / sin > tol):
+
+
           
+
      tv = np.linspace(tin, (3e6 * 365 * 24 * 3600), npts)
      y = odeint(f, [rin * cmperau, sin], tv)
 
-     for i in range(len(y[:,0]) - 1):
-          if y[:,0][i] <= y[:,0][i + 1]:
-               break
-          #sf = y[:,1][i - 1]
-          #npts = 10 * npts
+##     for i in range(len(y[:,0]) - 1):
+##          if y[:,0][i] <= y[:,0][i + 1]:
+##               break
+##          #sf = y[:,1][i - 1]
+##          #npts = 10 * npts
+     fint = interp1d(y[:,1][::-1], y[:,0][::-1])
 
+     try: 
+          return float(fint(0)) / cmperau
+     except ValueError:
+          return y[:,0][-1] / cmperau
      #Re = s * vrel / (lambdamfp(rH2O) * cdisk(rH2O))
-     return y[:,0][i - 1] / cmperau, y[:,1][i - 1]
+     #return y[:,0] / cmperau, y[:,1]
 
 
 def r_stop(s, mx, Ex, Nx = 1e15, rhos = 3.0, T0 = 120, betaT = 3./7, mu = 2.35, Sigma0 = 2200, betaS = 3./2, \
