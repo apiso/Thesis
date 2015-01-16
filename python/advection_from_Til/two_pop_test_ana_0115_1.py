f8c4d2347b1c801dfbf02f1c0e52ceadb3a85a9d
Ana-Maria Piso <ana-mariapiso@Ana-Marias-MacBook-Pro.local>
2015-01-15 21:46:04 -0500

    upd

--- python/advection_from_Til/two_pop_test_ana_0115_1.py
+++ python/advection_from_Til/two_pop_test_ana_0115_1.py
@@ -27,7 +27,7 @@ m_star        = M_star*ones(n_t)
 T_COAG_START  = 0.0
 RHO_S         = 1.6
 V_FRAG        = 0 #1000
-a_0           = 100.0
+a_0           = 0.1
 E_drift       = 1.0
 #
 # set the initial surface density & velocity
